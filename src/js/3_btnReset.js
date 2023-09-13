27fb1aeff855ba9df980277635bd08c31ae386e3
crisrodriguezgar <rodriguezgar.cris@gmail.com>
2023-09-13 19:10:46 +0200

    new changes

--- src/js/3_btnReset.js
+++ src/js/3_btnReset.js
@@ -2,12 +2,16 @@
 const btnReset = document.querySelector('.js-btn-reset');
 
 btnReset.addEventListener('click',(event)=>{
-    event.preventDefault();
-    nameInput.value = '';
-    jobInput.value = '';
-    emailInput.value = '';
-    telephoneInput.value = '';
-    linkedinInput.value = '';
-    gitInput.value = '';  
+  event.preventDefault();
+  nameInput.value = '';
+  jobInput.value = '';
+  emailInput.value = '';
+  telephoneInput.value = '';
+  linkedinInput.value = '';
+  gitInput.value = '';
+  namePreview.innerHTML ='Nombre Apellido';
+  jobPreview.innerHTML = 'Front-end developer';
 });
+
+
 
