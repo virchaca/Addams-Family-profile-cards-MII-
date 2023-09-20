bf70aa8a2237c2969112acbc53bc9b0303369215
crisrodriguezgar <rodriguezgar.cris@gmail.com>
2023-09-20 16:42:46 +0200

    update button reset color

--- src/js/0_global.js
+++ src/js/0_global.js
@@ -208,6 +208,9 @@ function handleClickDelete(event) {
   gitPreview.href = '#';
   namePreview.innerHTML ='Nombre Apellido';
   jobPreview.innerHTML = 'Front-end developer';
+  cardPreview.classList.add('paletteOne');
+  cardPreview.classList.remove('paletteTwo');
+  cardPreview.classList.remove('paletteThree');
 }
 
 btnReset.addEventListener('click', handleClickDelete);
